e8bf5418283878875f47311276c88269ba893c0e
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2020-09-20 10:09:52 +0900

    add setup.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='toukei',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    ],
+)
